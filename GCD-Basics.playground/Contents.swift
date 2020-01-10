d000e62b11d0dd0c928cc539be09da1d8decf1a1
Mohit Kumar <mohit.kumar@lalamove.com>
2020-01-10 14:30:52 +0530

    Add example for global queue

---
+++ GCD-Basics.playground/Contents.swift
@@ -0,0 +1,94 @@
+import UIKit
+
+/*
+ Main, global and custom queues
+ The main queue is a serial one, every task on the main queue runs on the main thread.
+
+ Global queues are system provided concurrent queues shared through the operating system. There are exactly four of them organized by high, default, low priority plus an IO throttled background queue.
+
+ Custom queues can be created by the user. Custom concurrent queues always mapped into one of the global queues by specifying a Quality of Service property (QoS). In most of the cases if you want to run tasks in parallel it is recommended to use one of the global concurrent queues, you should only create custom serial queues.
+ */
+
+/*
+============================================================================================================
+                                Type Of DispatchQueue
+============================================================================================================
+ */
+
+/*
+ ===========================================================================================================
+                                            1. MainQueue
+ ===========================================================================================================
+ */
+
+class MainQueueExample {
+    func executeTask() {
+        DispatchQueue.main.async {
+            // Perform main queue tasks here
+        }
+    }
+}
+
+
+/*
+=============================================================================================================
+                                           2.Global Queue
+ 
+ Global queues: concurrent queues that are shared by the whole system. There are four such queues with different priorities : high, default, low, and background. The background priority queue has the lowest priority and is throttled in any I/O activity to minimize negative system impact.
+=============================================================================================================
+
+ When sending tasks to the global concurrent queues, you don’t specify the priority directly. Instead, you specify a Quality of Service (QoS) class property. This indicates the task’s importance and guides GCD in determining the priority to give to the task.
+ 
+ Qos classes -
+ User-interactive - for imidiate, Use it for UI updates, event handling and small workloads that require low latency
+ User-initiated - The user initiates these asynchronous tasks from the UI. Use them when the user is waiting for immediate results and for tasks required to continue user interaction. They execute in the high priority global queue
+ Utility - Long running tasks, eg show loading icon
+ Background - This is lowest priority
+ 
+===============================================================================================================
+ */
+
+DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+  guard let self = self else {
+    return
+  }
+  let overlayImage = // get image data
+  DispatchQueue.main.async { [weak self] in // render
+  }
+}
+
+
+
+/*
+ =============================================================================================================
+                                            2.Custom Queue
+
+ queues that you create which can be serial or concurrent. Requests in these queues actually end up in one
+ of the global queues
+ =============================================================================================================
+ Read-Write Problem Solution using ------- barrier -------- barrier task will run alone rest will work concurrent
+ =============================================================================================================
+ */
+private let concurrentPhotoQueue =
+DispatchQueue(
+  label: "com.raywenderlich.GooglyPuff.photoQueue",
+  attributes: .concurrent)
+
+func addPhoto(_ photo: Photo) {
+  concurrentPhotoQueue.async(flags: .barrier) { [weak self] in
+    guard let self = self else {
+      return
+    }
+    self.unsafePhotos.append(photo)
+  }
+}
+
+var photos: [Photo] {
+  var photosCopy: [Photo]!
+  concurrentPhotoQueue.sync {
+    photosCopy = self.unsafePhotos
+  }
+  return photosCopy
+}
+
+
--- GCD-Group-Cancel-Simaphore-DispatchSource.playground/Contents.swift
+++ GCD-Group-Cancel-Simaphore-DispatchSource.playground/Contents.swift
@@ -121,3 +121,51 @@ var semaphoreTestObj = DispatchSemaphoreTest()
 semaphoreTestObj.fetchMultipleResponses {
     print("All group executed, Now we can execute further operations")
 }
+
+
+/*
+ ================================================================================================
+                        Observing changes in a file with DispatchSource
+ ================================================================================================
+ The final “lesser known” feature of GCD that I want to bring up is how it provides a way to observe changes in a file on the file system. Like DispatchSemaphore, this is something which can be super useful in a script or command line tool, if we want to automatically react to a file being edited by the user. This enables us to easily build developer tools that have “live editing” features.
+==================================================================================================
+ */
+
+class FileObserver {
+    private let file: File
+    private let queue: DispatchQueue
+    private var source: DispatchSourceFileSystemObject?
+
+    init(file: File) {
+        self.file = file
+        self.queue = DispatchQueue(label: "com.myapp.fileObserving")
+    }
+
+    func start(closure: @escaping () -> Void) {
+        // We can only convert an NSString into a file system representation
+        let path = (file.path as NSString)
+        let fileSystemRepresentation = path.fileSystemRepresentation
+
+        // Obtain a descriptor from the file system
+        let fileDescriptor = open(fileSystemRepresentation, O_EVTONLY)
+
+        // Create our dispatch source
+        let source = DispatchSource.makeFileSystemObjectSource(
+            fileDescriptor: fileDescriptor,
+            eventMask: .write,
+            queue: queue
+        )
+
+        // Assign the closure to it, and resume it to start observing
+        source.setEventHandler(handler: closure)
+        source.resume()
+        self.source = source
+    }
+}
+
+let observer = try FileObserver(file: file)
+
+observer.start {
+    print("File was changed")
+}
+
